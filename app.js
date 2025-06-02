8193ae6ed5377961cdb556363aec0172087dc2f9
nguyenvh60 <contact.tkl.fco@gmail.com>
2025-06-02 16:56:43 +0700

    Thay đổi nội dung

--- app.js
+++ app.js
@@ -1,18 +1,18 @@
 
-// always storage DOM elements in varibles on top because we won't need to repeat querySelector many times
+
 const userScoreSpan = document.querySelector("#user-score");
 const computerScoreSpan = document.querySelector("#computer-score");
 const scoreBoardDiv = document.querySelector(".score-board");
-const resultDiv = document.querySelector(".result > p"); // get the p tag inside the result class
+const resultDiv = document.querySelector(".result > p"); 
 const rockDiv = document.querySelector("#r");
 const paperDiv = document.querySelector("#p");
 const scissorsDiv = document.querySelector("#s");
 
-// default varibles, userScore and computerScore will be increased so we will use let to declare variable 
+
 let userScore = 0;
 let computerScore = 0;
 
-// function that takes value from individual buttons
+
 function main() {
     rockDiv.addEventListener("click", function () {
         game("r");
@@ -30,19 +30,15 @@ function main() {
 main();
 
 
-// make computer's choice (random choice between those 3 options)
 function getComputerChoice() {
     const choices = ["r", "p", "s"];
     const randomNumber = Math.floor(Math.random() * 3);
-    //Math is a build-in object in JS and random() and floor() are methods that exists in Math object
-    //random() gives random decimal numbers between 0 and 1
-    // floor() give rounding Numbers 0,1,2 , because the array only has 3 elements
     return choices[randomNumber];
 }
-//console.log(getComputerChoice());
 
 
-// compare user's choices againts computer's choices
+
+
 function game(userChoice) {
     const computerChoice = getComputerChoice();
     switch (userChoice + computerChoice) {
@@ -64,8 +60,7 @@ function game(userChoice) {
     }
 }
 
-// check who wins
-// then display the result back on to the DOM
+
 function win(userChoice, computerChoice) {
     userScore++;
     userScoreSpan.innerHTML = userScore;
@@ -82,7 +77,7 @@ function draw(userChoice, computerChoice) {
     resultDiv.innerHTML = "Hòa Rồi 🤗 ";
 }
 
-//convert
+
 function convertKeyWords(letter) {
     if (letter === "r") return "Búa";
     if (letter === "p") return "Bao";
